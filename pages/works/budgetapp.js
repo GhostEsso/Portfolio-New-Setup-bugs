d7103faea1c5b8bb9d8619b3b7aa7de704ade7e8
GhostEsso <lilipitaham@gmail.com>
2024-01-20 16:27:22 +0000

    allow budget app to open in new tab

--- pages/works/budgetapp.js
+++ pages/works/budgetapp.js
@@ -1,43 +1,51 @@
-import { Container, Badge, Link, ListItem, List } from "@chakra-ui/react";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Title, WorkImage, Meta } from "../../components/work";
-import P from "../../components/paragraph";
-import Layout from "../../components/layouts/article";
+import { Container, Badge, Link, ListItem, List } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
 
 const Work = () => {
-    return (
-        <Layout title="React Instagram Clone">
-            <Container>
-                <Title>
-                    Budget App (SmartPocket) <Badge>2023</Badge>
-                </Title>
-            </Container>
-            <P>
-            The SmartPocket app manages your expenses.
-                        You have the ability to make, change, or remove categories and transactions.
-                        Each transaction is linked to one or multiple categories.
-            </P>
-            <List ml={4} my={4}>
-                <ListItem>
-                    <Meta> WEBAPP </Meta>
-                    <Link href="https://moyin-3unn.onrender.com/">
-                        Webapp <ExternalLinkIcon mr={2} />
-                    </Link>
-                </ListItem>
-                <ListItem>
-                    <Meta> Github </Meta>
-                    <Link href="https://github.com/GhostEsso/my-budget-app">
-                    Source Code <ExternalLinkIcon mr={2} />
-                    </Link>
-                </ListItem>
-                <ListItem>
-                    <Meta> STACK </Meta>
-                    <span>Ruby on Rails</span>
-                </ListItem>
-            </List>
-            <WorkImage src="../images/works/budgetapp.png" alt="budgetapp" />
-        </Layout>
-    )
+  return (
+    <Layout title="React Instagram Clone">
+      <Container>
+        <Title>
+          Budget App (SmartPocket) <Badge>2023</Badge>
+        </Title>
+      </Container>
+      <P>
+        The SmartPocket app manages your expenses. You have the ability to make,
+        change, or remove categories and transactions. Each transaction is
+        linked to one or multiple categories.
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta> WEBAPP </Meta>
+          <Link
+            href="https://moyin-3unn.onrender.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Webapp <ExternalLinkIcon mr={2} />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta> Github </Meta>
+          <Link
+            href="https://github.com/GhostEsso/my-budget-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source Code <ExternalLinkIcon mr={2} />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta> STACK </Meta>
+          <span>Ruby on Rails</span>
+        </ListItem>
+      </List>
+      <WorkImage src="../images/works/budgetapp.png" alt="budgetapp" />
+    </Layout>
+  )
 }
 
-export default Work;
+export default Work
