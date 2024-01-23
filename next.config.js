fc297d55be85ba4497196253c4c8bf5b3e56caa3
GhostEsso <lilipitaham@gmail.com>
2024-01-23 09:44:35 +0000

    updated

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const NextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { dev, isServer }) => {
+    // Ajoutez des configurations spécifiques à la production ici
+    if (!dev && !isServer) {
+      // Exemple : Activer la minification uniquement en production
+      config.optimization.minimize = true;
+    }
+
+    return config;
+  },
 };
 
 module.exports = NextConfig;
