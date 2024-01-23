83bf477a24738cd59da2166bc6edaf993368bac8
GhostEsso <lilipitaham@gmail.com>
2024-01-23 14:58:52 +0000

    updated

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
 import NoSsr from '../no-ssr.js'
-import GamingSetup from '../gaming-setup.js'
+// import GamingSetup from '../gaming-setup.js'
 import Footer from '../footer.js'
 
 
@@ -28,7 +28,7 @@ const Main = ({ children, router }) => {
       <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
       <NoSsr>
-        <GamingSetup />
+        {/* <GamingSetup /> */}
       </NoSsr>
           {children}
           <Footer/>
